3d477fa390699c8ed340d8eaed4d4cd958b016c3
vissualy <ikimichristian@gmail.com>
2022-10-27 13:38:10 +0300

    update

--- python1.py
+++ python1.py
@@ -1,15 +1,17 @@
 import random
 
-user = int(input('from\n: '))
-for _ in range(user):
-    rand = random.randint(0,user)
-    print(rand)
-    
+print('provide amont to place bet')
+user1 = int(input('ammount\n: '))
+
 choice = ['rock','papper','sissors']
-print('make choice: rock,papper,sissors,)
-user = input('choice\n:)
+print('make choice: rock,papper,sissors,')
+user = input('choice\n:')
 answer = random.choices(choice)
 if user == answer:
-             print('won')
+    print('won')
+    bet = user1 * 2
+    print(f'+ {bet}')
 elif user != answer:
-             print('lost')
+    print('lost')
+    bet2 = user1 / 2
+    print(f'- {bet2}')
