2ba104961a9d2c93e5441f322f5f80cd8059a29d
christian gitahi azazua <116737660+vissualy@users.noreply.github.com>
2022-10-27 13:31:22 +0300

    update

--- python1.py
+++ python1.py
@@ -4,3 +4,12 @@ user = int(input('from\n: '))
 for _ in range(user):
     rand = random.randint(0,user)
     print(rand)
+    
+choice = ['rock','papper','sissors']
+print('make choice: rock,papper,sissors,)
+user = input('choice\n:)
+answer = random.choices(choice)
+if user == answer:
+             print('won')
+elif user != answer:
+             print('lost')
